6d1008bb4ccf24f99e281dccb6a08a449406619b
Jibon369-cyber <ahjibon466@gmail.com>
2026-09-24 21:45:11 +0600

    fix footer logo size

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -8,8 +8,8 @@ const Footer = () => {
         <div className='flex items-center gap-3'>
           <Image
             src='/logo.png'
-            width={40}
-            height={40}
+            width={30}
+            height={30}
             alt='FitLog logo'
             className='object-contain'
           />
